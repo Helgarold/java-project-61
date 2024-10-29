559f5a7c059be7fcd3c737a8c10b609aa79cb346
Helgarold <mipakk95@gmail.com>
2024-10-29 19:27:10 +0300

    Обновил App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greetUser();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                Cli.greetUser(); // Сохраняем приветствие
+                break;
+            case 2:
+                EvenGame.startGame(); // Запуск игры "Проверка на четность"
+                break;
+            case 0:
+                System.out.println("Exiting the game.");
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+                break;
+        }
+
+        scanner.close();
     }
 }
