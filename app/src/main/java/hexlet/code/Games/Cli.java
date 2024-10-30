aa652148186823190efaccfada458412322d75a3
Helgarold <mipakk95@gmail.com>
2024-10-30 18:27:24 +0300

    Обновление Cli.java

--- app/src/main/java/hexlet/code/Games/Cli.java
+++ app/src/main/java/hexlet/code/Games/Cli.java
@@ -11,33 +11,34 @@ public class Cli {
     }
 
     public String greetUser() {
-        System.out.print("Введите ваше имя: ");
+        System.out.print("Please enter your name: ");
         String name = scanner.nextLine();
         // Удаляем возможный остаток в буфере
         return name;
     }
 
     public void displayMenu() {
-        System.out.println("1 - Greetings");
-        System.out.println("2 - Even game");
-        System.out.println("3 - Calculator game");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calculator");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
     }
 
     public int getChoice() {
         while (true) {
-            System.out.print("Выберите действие: ");
+            System.out.print("Your choice: ");
             try {
                 int choice = scanner.nextInt();
-                if (choice >= 0 && choice <= 5) {
+                if (choice >= 0 && choice <= 6) {
                     return choice; // Позволяем 0 для выхода
                 } else {
-                    System.out.println("Некорректный ввод, выберите номер от 0 до 5.");
+                    System.out.println("Invalid input, please select a number from 0 to 6.");
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Некорректный ввод, пожалуйста, введите номер действия.");
+                System.out.println("Invalid input, please enter a number.");
                 scanner.nextLine(); // очищаем буфер
             }
         }
