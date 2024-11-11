1948756f9ade33f0c4ed68d035b0d8cd899cf9ca
Helgarold <mipakk95@gmail.com>
2024-11-11 17:53:56 +0300

    Update Cli.java

--- app/src/main/java/hexlet/code/games/Cli.java
+++ app/src/main/java/hexlet/code/games/Cli.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 /**
  * Класс, содержащий методы для взаимодействия с пользователем.
-        */
+ */
 public class Cli {
 
     /**
-            * Запрашивает имя пользователя и приветствует его.
-            *
-            * @param scanner объект типа Scanner для получения ввода от пользователя.
+     * Запрашивает имя пользователя и приветствует его.
+     *
+     * @param scanner объект типа Scanner для получения ввода от пользователя.
      */
     public static void greetUser(Scanner scanner) {
         System.out.print("May I have your name? ");
