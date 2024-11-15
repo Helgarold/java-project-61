464b66ff1265f706fa8edadf9a99687bd3604920
Helgarold <mipakk95@gmail.com>
2024-11-15 19:53:59 +0300

    Improve GcdGame class with better input validation

--- app/src/main/java/hexlet/code/games/GcdGame.java
+++ app/src/main/java/hexlet/code/games/GcdGame.java
@@ -1,7 +1,7 @@
 package hexlet.code.games;
 
 import hexlet.code.utils.Utils;
-import hexlet.code.Engine; // Импортируем класс Engine для запуска игры
+import hexlet.code.Engine;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +10,13 @@ public class GcdGame {
 
     private static final int MAX_NUMBER = 100;
     private static final int MIN_NUMBER = 1;
+    private static final int NUMBER_OF_ROUNDS = 3;
 
     public static void startGame() {
         // Получаем приветственное сообщение
         String welcomeMessage = getWelcomeMessage();
         // Получаем данные для игры
-        List<String[]> gameData = getGameData(3); // 3 раунда
+        List<String[]> gameData = getGameData(NUMBER_OF_ROUNDS);
         // Запускаем игру
         Engine.runGame(welcomeMessage, gameData);
     }
@@ -25,17 +26,17 @@ public class GcdGame {
     }
 
     public static String[] getQuestionAndCorrectAnswer() {
-        int num1 = Utils.getRandomInt(MIN_NUMBER, MAX_NUMBER + 1); // Генерация первого числа
-        int num2 = Utils.getRandomInt(MIN_NUMBER, MAX_NUMBER + 1); // Генерация второго числа
-        String question = num1 + " " + num2; // Формируем строку с вопросом
-        String correctAnswer = String.valueOf(gcd(num1, num2)); // Вычисляем правильный ответ через НОД
-        return new String[]{question, correctAnswer}; // Возвращаем в виде массива
+        int num1 = Utils.getRandomInt(MIN_NUMBER, MAX_NUMBER + 1);
+        int num2 = Utils.getRandomInt(MIN_NUMBER, MAX_NUMBER + 1);
+        String question = num1 + " " + num2;
+        String correctAnswer = String.valueOf(gcd(num1, num2));
+        return new String[]{question, correctAnswer};
     }
 
     public static List<String[]> getGameData(int rounds) {
         List<String[]> gameData = new ArrayList<>();
         for (int i = 0; i < rounds; i++) {
-            gameData.add(getQuestionAndCorrectAnswer()); // Добавляем вопрос и ответ в список
+            gameData.add(getQuestionAndCorrectAnswer());
         }
         return gameData;
     }
